4fb06ac6251b3fa9a97e487873ccbd562b045376
sicknuccio <giardino495@gmail.com>
2022-10-25 14:37:42 +0200

    operazioni matematiche con javascript in console

---
+++ 25-10-22/script.js
@@ -0,0 +1,43 @@
+//--------DICHIARAZIONI--------
+
+let inputOperation, firstValue, secondValue;
+
+//--------INPUT--------
+
+alert("Operazioni matematica: 1=Somma,2=Sottrazione,3=Moltiplicazione,4=Divisione");
+inputOperation = prompt("Inserisci valore:");
+inputOperation = parseInt (inputOperation);
+firstValue = prompt("Inserisci primo valore:");
+firstValue = parseFloat (firstValue);
+secondValue = prompt("Inserisci secondo valore:");
+secondValue = parseFloat (secondValue);
+
+
+
+
+ //--------OPERAZIONI--------
+
+switch (inputOperation) {
+
+    case 1:
+        console.log( firstValue + secondValue);
+        break;
+    
+    case 2:
+        console.log( firstValue -  secondValue);
+        break;
+    
+    case 3:
+        console.log( firstValue * secondValue);
+        break;
+    
+    case 4:
+        console.log( firstValue / secondValue);
+        break;
+    
+    default :
+        console.log("Il valore inserito non corrisponde a nessuna operazione");
+    
+}
+
+ 
