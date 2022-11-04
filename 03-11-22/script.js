cf7d5f37de0a742c05a829fae47854bf2da1d413
sicknuccio <giardino495@gmail.com>
2022-11-04 11:28:48 +0100

    update_1

--- 03-11-22/script.js
+++ 03-11-22/script.js
@@ -4,7 +4,7 @@ const bodyEl = document.body;
 //CREAZIONE HERO
 const heroEl = document.createElement("div");
 heroEl.className = "hero";
-heroEl.textContent = "TO DO LIST:ANIMAZIONE SUL PUNTATORE SULL'IMMAGINE,CONTINUARE A MIGLIORARE LA RESPONSIVENESS E MIGLIORARE LA GRAFICA,BOX PER UTENTE DOVE INSERIRE I DATI PER ACCEDERE";
+heroEl.textContent = "TO DO LIST:ANIMAZIONE SUL PUNTATORE SULL'IMMAGINE,CONTINUARE A MIGLIORARE LA RESPONSIVENESS E MIGLIORARE LA GRAFICA,BOX PER UTENTE DOVE INSERIRE I DATI PER ACCEDERE,FILTRARE GLI OGGETTI IN BASE AL PULSANTE CHE CLICCA L'UTENTE";
 //CREAZIONE BOTTONE DARKMODE
 const buttonEl = document.createElement("button");
 buttonEl.textContent ="Dark Mode";
@@ -37,12 +37,24 @@ characters.map((character) => createCard(character));
 //CREAZIONE DARKMODE TRAMITE BOTTONE
 buttonEl.addEventListener("click", () => bodyEl.classList.toggle("darkmode"));
 
-//CREAZIONE BOX PER LOGIN
+//CREAZIONE BOX PER LOGIN WORK IN PROGRESS
 
 // const boxLogEl = document.createElement("div");
 // boxLogEl.className = "loginBox";
 // const boxTitle = document.createElement("h2");
 // const boxForm = document.createElement("form");
 
+//FILTRARE OGGETTI  
+
+
+characters
+.filter((character) => character.name == "Goku" )
+.map((character)=>{
+    createCard(
+        character.name,
+        character.image,
+        character.description,
+    )
+});
 
 
