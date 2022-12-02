7245161d44e2234234d5eb0b904cb6ba751e0e3a
sicknuccio <giardino495@gmail.com>
2022-12-02 17:15:38 +0100

    Middleware

---
+++ 02-12-22/public/js/edit_directors.js
@@ -0,0 +1,118 @@
+const GET = async (id) => {
+  const url = `http://localhost:3000/regista?id=${id}`;
+  const res = await fetch(url);
+  return await res.json();
+};
+
+const PUT = async (body) => {
+  const url = `http://localhost:3000/regista`;
+  return await fetch(`${url}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    body: new URLSearchParams(body),
+  });
+};
+
+const DELETE = async (body) => {
+  const url = `http://localhost:3000/regista`;
+  return await fetch(`${url}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    body: new URLSearchParams(body),
+  });
+};
+
+const search_btn = document.getElementById("search-btn");
+const idEl = document.getElementById("id");
+
+const container = document.getElementById("container");
+
+/**
+ * Eventi
+ */
+search_btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  GET(idEl.value).then((regista) => {
+    container.textContent = "";
+    createCard(regista);
+  });
+});
+
+/**
+ *
+ * Creazione Card
+ */
+const createCard = (actor) => {
+  const cardEl = document.createElement("div");
+  const infoEl = document.createElement("h2");
+  const nameEl = document.createElement("input");
+  const surnameEl = document.createElement("input");
+  const data_nascitaEl = document.createElement("input");
+  const saveEl = document.createElement("button");
+  const deleteEl = document.createElement("button");
+
+  infoEl.textContent = "Informazioni";
+  cardEl.classList = "card";
+
+  nameEl.classList = "actor_name";
+  nameEl.setAttribute("placeholder", actor.nome);
+
+  surnameEl.classList = "actor_surname";
+  surnameEl.setAttribute("placeholder", actor.cognome);
+
+  data_nascitaEl.classList = "actor_data_nascita";
+  data_nascitaEl.setAttribute("placeholder", actor.data_nascita);
+
+  saveEl.classList = "save-btn";
+  saveEl.textContent = "Salva";
+
+  deleteEl.classList = "delete-btn";
+  deleteEl.textContent = "Elimina";
+
+  nameEl.textContent = "Nome: " + actor.nome;
+  surnameEl.textContent = "Cognome: " + actor.cognome;
+  data_nascitaEl.textContent = "Data di nascita: " + actor.data_nascita;
+
+  /**
+   * Eventi bottoni card
+   */
+
+  saveEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    let upRegista = {
+      id: actor.id,
+      nome: nameEl.value == "" ? actor.nome : nameEl.value,
+      cognome: surnameEl.value == "" ? actor.cognome : surnameEl.value,
+      data_nascita:
+        data_nascitaEl.value == "" ? actor.data_nascita : data_nascitaEl.value,
+      // riconoscimenti: riconoscimentiEl.value,
+      // inizio_attivita: inizio_attivitaEl.value,
+      // fine_attivita: fine_attivitaEl.value,
+      // in_attivita: in_attivitaEl.value,
+    };
+    console.log(
+      PUT(upRegista).then((res) => {
+        console.log(res);
+      })
+    );
+  });
+
+  deleteEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    let body = {
+      id: actor.id,
+    };
+    console.log(
+      DELETE(body).then((res) => {
+        console.log(res);
+      })
+    );
+  });
+
+  cardEl.append(infoEl, nameEl, surnameEl, data_nascitaEl, saveEl, deleteEl);
+  container.append(cardEl);
+};
